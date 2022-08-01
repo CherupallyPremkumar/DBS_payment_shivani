a2d8d20456f986ce95580ac0c1837c92cfbf2cdf
premkumar <premkumar.cherupally@atyeti.com>
2022-08-01 10:47:08 +0530

    first Commit maven

---
+++ DBS_payment/src/main/java/com/example/DBS_payment/DbsPaymentApplication.java
@@ -0,0 +1,13 @@
+package com.example.DBS_payment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbsPaymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbsPaymentApplication.class, args);
+	}
+
+}
---
+++ DBS_payment/src/test/java/com/example/DBS_payment/DbsPaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DBS_payment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbsPaymentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
